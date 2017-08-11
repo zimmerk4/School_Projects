8f42b077e750ba84d9893182c87194d1f967e790
zimmerk4 <zimmerk4@msu.edu>
2017-08-11 04:25:33 -0400

    Rook Polynomials are hard...

--- Rook_Polynomials.py
+++ Rook_Polynomials.py
@@ -1,3 +1,69 @@
+import copy
+import math
+
+
+def binomial(n, k):
+    if n > 0:
+        return int(math.factorial(n)/(math.factorial(k) * math.factorial(n - k)))
+    elif n == 0:
+        return 0
+    elif n < 0:
+        return None
+
+class Polynomial:
+    def __init__(self, c):
+        self.coefs = c
+
+    def __repr__(self):
+        prnt_str = ""
+        for deg, coef in enumerate(self.coefs):
+            if coef > 0 and deg != 0:
+                prnt_str += " + " + str(coef) + "*x^" + str(deg)
+            elif coef > 0 and deg == 0:
+                prnt_str += str(coef) + "*x^" + str(deg)
+            elif coef < 0:
+                prnt_str += " - " + str(-coef) + "*x^" + str(deg)
+        if prnt_str != "" and (prnt_str[1] == '+' or prnt_str[1] == '1'):
+            prnt_str = prnt_str[3:]
+        return prnt_str
+
+    def __add__(self, other):
+        poly = Polynomial([])
+        for i in range(len(other.coefs)):
+            if i < len(self.coefs):
+                poly.coefs.append(self.coefs[i] + other.coefs[i])
+            else:
+                poly.coefs.append(other.coefs[i])
+        return poly
+
+    def __sub__(self, other):
+        poly = Polynomial([])
+        for i in range(len(other.coefs)):
+            if i < len(self.coefs):
+                poly.coefs.append(self.coefs[i] - other.coefs[i])
+            else:
+                poly.coefs.append(0-other.coefs[i])
+        return poly
+
+    def __mul__(self, other):
+        poly = Polynomial([0] * (len(self.coefs) + len(other.coefs) - 1))  # Length is sum of highest degree + 1
+        for deg1, coef1 in enumerate(self.coefs):
+            for deg2, coef2 in enumerate(other.coefs):
+                # Multiplies coefficients and places them in the index related to
+                # the sum of the degrees of x
+                poly.coefs[deg1 + deg2] += coef1 * coef2
+        return poly
+
+    def __len__(self):
+        return len(self.coefs)
+
+    def degree(self):
+        poly = self.coefs[:]
+        while poly[-1] == 0:
+            poly.pop()
+        return len(poly) - 1
+
+
 class Board:
     def __init__(self, h, w, bad_sqrs):
         self.height = h
@@ -6,11 +72,10 @@ class Board:
         for i in range(h):
             self.board[i] = {}
             for j in range(w):
-                if (i, j) in bad_sqrs or (j, i) in bad_sqrs:
+                if (i, j) in bad_sqrs:
                     self.board[i][j] = 0
                 else:
                     self.board[i][j] = 1
-        return
 
     def __repr__(self):
         prnt_str = ""
@@ -20,56 +85,43 @@ class Board:
             prnt_str += "\n"
         return prnt_str
 
+    def build_B_i_and_B_e(self):
+        B_i = Board(self.height, self.width, {})
+        B_i.board = copy.deepcopy(self.board)
+        B_e = Board(self.height, self.width, {})
+        B_e.board = copy.deepcopy(self.board)
+        for i in range(len(B_i.board)):
+            for j in range(len(B_i.board)):
+                if B_i.board[i][j]:
+                    for row in range(len(B_i.board)):
+                        if i == row:
+                            for val in B_i.board[row]:
+                                B_i.board[row][val] = 0
+                        B_i.board[row][j] = 0
+                    B_e.board[i][j] = 0
+                    return B_i, B_e
+
     def find_rook_polynomial(self):
-        rook_polynomial = []
-        return rook_polynomial
+        R_of_B = Polynomial([])
+        if len(self.board) == 1:
+            vals = [v for v in self.board.values()]
+            if len(vals[0]) == 1:
+                if len(self.board) == 0:
+                    return Polynomial([1])
+                for k in range(self.height + 1):
+                    R_of_B.coefs.append(binomial(self.height, k)*binomial(self.height, k)*math.factorial(k))
+                return R_of_B
+        B_i, B_e = self.build_B_i_and_B_e()
+        R_of_B = B_i.find_rook_polynomial() + B_e.find_rook_polynomial()
+        return R_of_B
 
 
-board = Board(2, 2, {})
+board = Board(2, 2, {(0,1)})
 print(board)
+# print()
+# b_i, b_e = board.build_B_i_and_B_e()
+# print(b_e)
+# print()
+# print(b_i)
 print()
 print(board.find_rook_polynomial())
-
-
-_pc = []
-for i in range(256):
-    c = 0
-    while i:
-        # clear last set bit
-        i &= i-1
-        c += 1
-    _pc.append(c)
-
-def popcount(i):
-    "Return number of bits set."
-    result = 0
-    while i:
-        result += _pc[i & 0xff]
-        i >>= 8
-    return result
-
-def perm(a):
-    "Return permanent of 0-1 matrix.  Each row is an int."
-    result = 0
-    n = len(a)
-    for s in range(1 << n):
-        prod = 1
-        for row in a:
-            prod *= popcount(row & s)
-        if popcount(s) & 1:
-            result -= prod
-        else:
-            result += prod
-    if n & 1:
-        result = -result
-    return result
-
-def matrix2ints(a):
-    return [int("".join(map(str, row)), 2)
-            for row in a]
-
-def matrix_perm(a):
-    "Return permanent of 0-1 matrix."
-    return perm(matrix2ints(a))
-
-print(matrix_perm([[1,1,0,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]))
